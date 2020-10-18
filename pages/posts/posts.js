50efcfe07a7ea86c6a66952178e18e35294dcb1d
爱做梦的袁大事 <oncwnuARQZexvg5c0mK5Lmzmt0uY@git.weixin.qq.com>
2020-10-18 15:43:04 +0800

    5-10 数据绑定的实战应用

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -16,12 +16,25 @@ Page({
    */
   onLoad: function (options) {
 
-    // setData接受的是一个javascript对象
-    // setData会将要修改或者增加的数据 放入data:{}中
-    // 有创建和更新的作用
-    this.setData({
-      title:"2021"
-    })
+   var  content={
+    authorImgsSrc:'/images/avatar/1.png',
+     date:'Nov 20 2020',
+     title:"《金刚川》志愿军没有退路",
+     jianjieimgsrc:"/images/jingangchuan.png",
+     jianjiecontent:"以三个视点讲述一个故事：工兵桥修好又被炸毁再度修好……一个屹立不倒、令敌人费解的战争奇迹",
+     collection:92,
+     reading:182,
+  
+    
+     detail:"",
+     dateTime:'24小时前',
+     headImgSrc:'',
+     author:'',
+     avatar:"",
+     postId:3
+   }
+    this.setData(content);
+  
 
   },
 
@@ -29,28 +42,29 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面隐藏
+   * 条件触发
    */
   onHide: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+   
   },
 
   /**
