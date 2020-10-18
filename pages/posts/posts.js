e701aa4f3f95460a90c5f9accaf8fee9ddc6e1b9
爱做梦的袁大事 <oncwnuARQZexvg5c0mK5Lmzmt0uY@git.weixin.qq.com>
2020-10-18 17:52:52 +0800

    5-11 Mustache语法解析

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -6,7 +6,7 @@ Page({
    */
   data: {
     // 初始值  所有页面所有要被绑定的变量都要在这里初始化
-    title: "《金刚川》志愿军没有退路"
+    title: "《金刚川》志愿军没有退路",
   },
 
   /**
@@ -16,25 +16,32 @@ Page({
    */
   onLoad: function (options) {
 
-   var  content={
-    authorImgsSrc:'/images/avatar/1.png',
-     date:'Nov 20 2020',
-     title:"《金刚川》志愿军没有退路",
-     jianjieimgsrc:"/images/jingangchuan.png",
-     jianjiecontent:"以三个视点讲述一个故事：工兵桥修好又被炸毁再度修好……一个屹立不倒、令敌人费解的战争奇迹",
-     collection:92,
-     reading:182,
+    //要传输的数据 
+    var content = {
+      authorImgsSrc: '/images/avatar/1.png',
+      date: 'Nov 20 2020',
+      title: "《金刚川》志愿军没有退路",
+      jianjieimgsrc: "/images/jingangchuan.png",
+      jianjiecontent: "以三个视点讲述一个故事：工兵桥修好又被炸毁再度修好……一个屹立不倒、令敌人费解的战争奇迹",
+      dataNum:{
+        collection:92,
+        reading: 182,
+      },
+       
   
-    
-     detail:"",
-     dateTime:'24小时前',
-     headImgSrc:'',
-     author:'',
-     avatar:"",
-     postId:3
-   }
+      detail: "",
+      dateTime: '24小时前',
+      headImgSrc: '',
+      author: '',
+      avatar: "",
+      postId: 3,
+      a:2,
+      b:3
+    }
     this.setData(content);
-  
+    this.setData({
+      sum:content.a+content.b
+    });
 
   },
 
@@ -42,14 +49,14 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
@@ -57,14 +64,14 @@ Page({
    * 条件触发
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-   
+
   },
 
   /**
