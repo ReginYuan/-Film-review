cf1a98f3ad3b5360515846bd39238d018f6fe4b6
爱做梦的袁大事 <oncwnuARQZexvg5c0mK5Lmzmt0uY@git.weixin.qq.com>
2020-10-18 12:59:28 +0800

    5-6 DOM优先 VS 数据优先

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -5,13 +5,24 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    title: "《金刚川》志愿军没有退路"
   },
 
   /**
    * 生命周期函数--监听页面加载
+   * 钩子函数 hook  function   
+   * 程序运行就会监听此方法内的变化
    */
   onLoad: function (options) {
+    console.log("袁修飞");
+
+    // DOM优先  获取属性id以后才可以改变标签的值和显示
+    // a = "金剛川";
+    // var d = document.getElementById('html属性id');
+    // d.innerHTML = a;
+
+    //数据优先 ->数据绑定  可以随意在页面任何一个地方显示{{d}}  数据驱动
+
 
   },
 
